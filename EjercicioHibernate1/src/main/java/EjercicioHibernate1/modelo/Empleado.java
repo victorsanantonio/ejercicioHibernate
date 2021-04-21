76bd20f7a2cdf80cc469be18f3a4f2e1ff93c7f7
victorsanantonio <victorsanantonio@hotmail.com>
2021-04-21 16:24:02 +0200

    Clase empleado mapeada

--- EjercicioHibernate1/src/main/java/EjercicioHibernate1/modelo/Empleado.java
+++ EjercicioHibernate1/src/main/java/EjercicioHibernate1/modelo/Empleado.java
@@ -1,5 +1,119 @@
 package EjercicioHibernate1.modelo;
 
 public class Empleado {
+	private int codigo;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String lugarNacimiento;
+	private String fechaNacimiento;
+	private String direccion;
+	private String telefono;
+	private String puesto;
+	private int codDepartamento;
+	
+	public Empleado() {}
 
+	public Empleado(int codigo, String nombre, String apellido1, String apellido2, String lugarNacimiento,
+			String fechaNacimiento, String direccion, String telefono, String puesto, int codDepartamento) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+		this.lugarNacimiento = lugarNacimiento;
+		this.fechaNacimiento = fechaNacimiento;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.puesto = puesto;
+		this.codDepartamento = codDepartamento;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public String getLugarNacimiento() {
+		return lugarNacimiento;
+	}
+
+	public void setLugarNacimiento(String lugarNacimiento) {
+		this.lugarNacimiento = lugarNacimiento;
+	}
+
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public int getCodDepartamento() {
+		return codDepartamento;
+	}
+
+	public void setCodDepartamento(int codDepartamento) {
+		this.codDepartamento = codDepartamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [codigo=" + codigo + ", nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2="
+				+ apellido2 + ", lugarNacimiento=" + lugarNacimiento + ", fechaNacimiento=" + fechaNacimiento
+				+ ", direccion=" + direccion + ", telefono=" + telefono + ", puesto=" + puesto + ", codDepartamento="
+				+ codDepartamento + "]";
+	}
 }
